b9817f66a9926b90000310f76d3ef7146cff1abe
transang06 <69206565+transang06@users.noreply.github.com>
2020-08-05 00:18:45 +0700

    Update ketnoi_db.php

--- ketnoi_db.php
+++ ketnoi_db.php
@@ -5,7 +5,6 @@ $password = "";
 $database = "quanlykhachsan";
 $ketnoisql = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_errno()) {
-    echo "Connection Fail: " . mysqli_connect_errno();
     echo "1225";
     exit;
 }
